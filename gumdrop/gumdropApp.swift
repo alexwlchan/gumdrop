a519844725405d84134ffa8979fcf15dfb51fe45
Alex Chan <alex@alexwlchan.net>
2023-10-22 14:54:34 +0100

    Initial Commit

---
+++ gumdrop/gumdropApp.swift
@@ -0,0 +1,17 @@
+//
+//  gumdropApp.swift
+//  gumdrop
+//
+//  Created by Alex Chan on 22/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct gumdropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
